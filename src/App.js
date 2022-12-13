813bd741db4a653b416002f4c4ccc20ca8046b39
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-13 16:25:49 +0300

    files changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useCallback } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  //*** dependency array içerisine "number" değişkenini verdiğimizde her defasında gereksiz render işlemi olur. Bu nedenle "prevState" kullandık. */
+  const increment = useCallback(() => {
+    setNumber((prevState) => prevState + 1);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header increment={increment} />
+
+      <hr />
+
+      <h1>{number}</h1>
     </div>
   );
 }
